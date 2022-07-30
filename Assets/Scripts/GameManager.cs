abe5341f0b26c0d70b1c4175b473488d49c9908a
hcorpaf <50888947+hcorpaf@users.noreply.github.com>
2022-07-30 13:51:29 +0200

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
-    int politicsStat;
-    int moneyStat;
-    int trueStat;
+    
+    public int maxLevelValue=10;
+    int pressureLevel=0;
+    int moneyLevel=5;
+    int credibilityLevel=5;
+    int elisaStrikes=0;
+    bool lastLevelCompleted=false;
 
     public string verb;
     public GameObject verbObject;
@@ -20,11 +23,12 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
 
-    public void PublishNewsPaper(int _politics, int _money, int _true)
+    public void PublishNewsPaper(int _politics, int _money, int _true, int _elisaStrike)
     {
-        politicsStat += _politics;
-        moneyStat += _money;
-        trueStat += _true;
+        pressureLevel += _politics;
+        moneyLevel += _money;
+        credibilityLevel += _true;
+        elisaStrikes += _elisaStrike;
     }
 
     public void EditNewsPaper(string _verb,GameObject _verbObject)
@@ -33,4 +37,34 @@ public class GameManager : MonoBehaviour
         verbObject = _verbObject;
     }
 
+    public void CompletedGame()
+    {
+        lastLevelCompleted=true;
+    }
+    
+    // Gestionar finales en funcion de los valores de los niveles de las métricas
+    public CriticalEndings(){
+        if (moneyLevel <= 0){
+            // Final por pérdida de dinero: quiebra del periódico
+            if (allTrue) AllwaysTrueEnding(); // Despido con conciencia tranquila
+            else LowMoneyEnding();
+        } else if (credibilityLevel <= 0){
+            // Final por pérdida de credibilidad: despido del periódico
+            LowCredibilityEnding();
+        } else if (pressureLevel >= maxLevelValue){
+            // Final por ganancia de presión: Muerte misteriosa en un accidente y reemplazo en el periódico
+            HighPressureEnding();
+        }
+    }
+
+    // Gestionar finales en funcion de los valores de los niveles de las métricas
+    public AlternativeEndings(){
+        if (elisaStrikes >= 2){
+            // Final Elisa es despedida: 2 noticias que perjudiquen a Elisa
+            ElisaFiredEnding();
+        } else if (lastLevelCompleted){
+            // Final completando todos los niveles: vida mediocre en el periódico
+            MediocreEnding();
+        }
+    }
 }
